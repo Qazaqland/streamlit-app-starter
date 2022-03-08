920196de5d8c55d8aef65719043c552010495af3
Qazaqland <99110727+Qazaqland@users.noreply.github.com>
2022-03-08 13:01:28 +0000

    changed ...

--- app.py
+++ app.py
@@ -9,3 +9,11 @@ st.image("https://unpackai.github.io/unpackai_logo.svg")
 st.title("WebScrapping of Lego Prices around the world 🌏")
 st.write("*by Bota*")
 # st.write("---")
+
+st.button("Launch baloons", on_click=st.balloons)
+
+st.button("Let it snow",on_click=st.snow)
+
+students=st.radio("Class",["High School","Middle School","Elementary"])
+number_of_students=st.slider("Number of students",1,35 )
+st.write("There are", number_of_students, "students")
