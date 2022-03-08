83449d3e7e2e3341107c5b310c42f3fedf2a0db2
Qazaqland <99110727+Qazaqland@users.noreply.github.com>
2022-03-08 12:38:22 +0000

    changed a name

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import streamlit as st
 
 import get_data as data
 
-st.set_page_config(page_title="WebScrapping - PY101", page_icon="🛒", layout="wide")
+st.set_page_config(page_title="WebScrapping - PY101", page_icon="🌏", layout="wide")
 st.image("https://unpackai.github.io/unpackai_logo.svg")
 st.title("WebScrapping of Lego Prices around the world 🌏")
-st.write("*by <name>*")
+st.write("*by Bota*")
 # st.write("---")
